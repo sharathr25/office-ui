4077f9e2043c1228bf0a521b911bf919e6e5df9f
sharathr25 <sharathdupati@gmail.com>
2022-05-26 13:32:46 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import axios from 'axios'
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect
-} from "react-router-dom";
-import { Login, Home, Admin } from './routes';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import { Login, Home, Admin } from './routes'
 
 const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/login" exact>
-                    <Login />
-                </Route>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/admin" exact>
-                    <Admin />
-                </Route>
-                <Route render={() => <Redirect to="/" />} />
-            </Switch>
-        </Router>
-    );
+  useEffect(() => {
+    window
+      .fetch('http://localhost:8080/students', {
+        headers: { Authorization: '1234' }
+      })
+      .then(res => res.text())
+      .then(text => console.log(JSON.parse(text)))
+  }, [])
+  return (
+    <Router>
+      <Switch>
+        <Route path='/login' exact>
+          <Login />
+        </Route>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/admin' exact>
+          <Admin />
+        </Route>
+        <Route render={() => <Redirect to='/' />} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
